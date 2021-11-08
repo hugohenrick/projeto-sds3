8678e8cf980aee310699d9f59b4c71472927b64d
Hugo Henrick <hugohenrick16@gmail.com>
2021-11-07 22:40:30 -0300

    Pagination

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -9,18 +9,20 @@ const Dashboard = () => {
             <NavBar />
             <div className="container">
                 <h1 className="text-primary py-3"> Dashboard de Vendas</h1>
+
                 <div className="row px-3">
                     <div className="col-sm-6">
                         <h5 className="text-center text-secondary">Taxa de Sucesso(%)</h5>
                         <BarChart />
                     </div>
+                    
                     <div className="col-sm-6">
                         <h5 className="text-center text-secondary">Participação nas Vendas</h5>
                         <DonutChart />
                     </div>
                 </div>
 
-                <div className="row px-3">
+                <div className="py-3">
                     <h2 className="text-primary">Todas as Vendas</h2>
                 </div>
                 <DataTable />
